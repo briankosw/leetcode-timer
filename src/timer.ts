4e0d4fcd55a1afa1d5920bafe9d8964fd07ab97c
Brian Ko <briankosw@gmail.com>
2022-02-01 13:38:14 +0900

    Add milliseconds precision to timer calculation

--- src/timer.ts
+++ src/timer.ts
@@ -15,15 +15,21 @@ export default class Timer {
     this.isRunning = true;
     this.startTime = Date.now()
     this.interval = setInterval(() => {
-      const elapsedSeconds = Math.floor((Date.now() - this.startTime) / 1000);
-      const hours = Math.floor(elapsedSeconds / 3600);
-      const minutes = Math.floor(elapsedSeconds / 60);
-      const seconds = elapsedSeconds % 60
+      const elapsedMilliseconds = Date.now() - this.startTime;
+      const [hours, minutes, seconds] = Timer.calculateHMS(elapsedMilliseconds);
       this.callback(hours, minutes, seconds)
     }, 100);
   }
 
   stop = () => {
     clearInterval(this.interval);
+  }
+
+  static calculateHMS(elapsedMilliseconds: number) {
+    const elapsedSeconds = Math.floor(elapsedMilliseconds / 1000);
+    const hours = Math.floor(elapsedSeconds / 3600);
+    const minutes = Math.floor(elapsedSeconds / 60);
+    const seconds = elapsedSeconds % 60;
+    return [hours, minutes, seconds];
   }
 }
